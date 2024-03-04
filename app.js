7d885ed78b4f660decdb36dd05dfe1153f31a494
Nicholas L <NLoo1@outlook.com>
2024-03-04 15:33:14 -0500

    Initial commit, finish part 1

---
+++ app.js
@@ -0,0 +1,31 @@
+// PART 1
+
+const factsList = document.querySelector('#facts')
+
+let fourFacts = [];
+let num = prompt("Please enter a number", 42)
+
+if(num != null && num !=""){
+    for (let i = 1; i < 5; i++) {
+        fourFacts.push(
+          axios.get(`http://numbersapi.com/${num}`)
+        );
+      }
+      
+      Promise.all(fourFacts)
+        .then(facts => {
+          for (res of facts) {
+            console.log(res.data)
+            const node = document.createElement("li");
+            const textnode = document.createTextNode(res.data);
+            node.appendChild(textnode);
+            factsList.appendChild(node);
+          }
+        })
+        .catch(err => console.log(err));
+}
+else{
+    console.log("No input received.")
+}
+
+// PART 2: DECK OF CARDS
